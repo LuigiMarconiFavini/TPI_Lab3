0c0fd7740306bbf665a20f4924f3396bb07ca010
BRUZZO.LUCA <luca.bruzzo99@gmail.com>
2024-05-22 19:50:39 -0300

    Hard coding SMARTPHONES array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
 import './App.css'
+
+const SMARTPHONES = [
+  {
+    name: "iPhone 13 Pro",
+    brand: "Apple",
+    imageUrl: "",
+  },
+  {
+    name: "Samsung Galaxy S24 Ultra",
+    brand: "Samsung",
+    imageUrl: "",
+  },
+  {
+    name: "Google Pixel 6 Pro",
+    brand: "Google",
+    imageUrl: "",
+  },
+  {
+    name: "Motorola Edge 20 Pro",
+    brand: "Motorola",
+    imageUrl: " ",
+  },
+];
 
 function App() {
 
   return (
     <>
-    <h1>  </h1>
+    
     </>
   )
 }
